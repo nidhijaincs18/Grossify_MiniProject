2920b1d9fad2dd5c782443728608a38951970868
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2020-10-17 15:24:09 +0530

    connected the firebase

--- app/src/main/java/com/example/grossify/LoginPage.java
+++ app/src/main/java/com/example/grossify/LoginPage.java
@@ -1,15 +1,44 @@
 package com.example.grossify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginPage extends AppCompatActivity {
+
+    TextView doNotHaveAccount, signUpLink, logoText, sloganText;
+    Button logIn;
+    TextInputLayout phone, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.login_page);
+
+        //hooks
+        logoText = findViewById(R.id.welcome_log_into);
+        sloganText = findViewById(R.id.login_slogan);
+        phone = findViewById(R.id.phone);
+        password = findViewById(R.id.password);
+        logIn = findViewById(R.id.login_btn);
+        doNotHaveAccount = findViewById(R.id.sign_up);
+        signUpLink = findViewById(R.id.sign_up_link);
+
+
+        signUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WelcomeSignUpPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/grossify/WelcomeSignUpPage.java
+++ app/src/main/java/com/example/grossify/WelcomeSignUpPage.java
@@ -1,15 +1,57 @@
 package com.example.grossify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class WelcomeSignUpPage extends AppCompatActivity {
+    //Variables
+    TextInputLayout name, email, phone, password;
+    Button signUpBtn, loginBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.welcome_sign_up_page);
+
+        name = findViewById(R.id.reg_name);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+        password = findViewById(R.id.password);
+        signUpBtn = findViewById(R.id.signup_btn);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginPage.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+                //Get the values
+
+
+                reference.setValue("First Data");
+            }
+        });
     }
 }
